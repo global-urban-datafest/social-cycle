8efca23e97902e70bf56c18282d29b58ec41ae37
John Maguire <jonnie.maguire@gmail.com>
2015-03-17 19:49:28 +0000

    removing unnessary files

--- App/ionicApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/John/Projects/fablab/social-cycle/App/ionicApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/John/Projects/fablab/social-cycle/App/ionicApp/platforms/android/CordovaLib/AndroidManifest.xml \
--- App/ionicApp/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/John/Projects/fablab/social-cycle/App/ionicApp/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/John/Projects/fablab/social-cycle/App/ionicApp/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/John/Projects/fablab/social-cycle/App/ionicApp/platforms/android/ant-build/classes.dex \
-/Users/John/Projects/fablab/social-cycle/App/ionicApp/platforms/android/CordovaLib/ant-build/classes.jar \
